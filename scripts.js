d0f70ced5f1ea8b0224a38b1abd65e730c398063
Ben Harker <har05054@byui.edu>
2017-03-29 10:54:11 -0600

    added support to click extension button to trigger calculator, along with syling tweaks

--- scripts.js
+++ scripts.js
@@ -1,58 +1,133 @@
 /*eslint-env browser*/
-function whatIf() {
-  var ou = top.location.search.split('=')[1];
-  var grades, gradeobjs;
-  document.getElementById('whatIf').style = "";
 
+function whatIf() {
+  if (document.getElementById('grade').innerText == "") {
+    document.getElementById('whatIf').style = "";
+    getSetup();
+  } else {
+    document.getElementById('whatIf').style = "";
+  }
+}
+
+function getSetup() {
+  var setup;
+  var ou = top.location.search.split('=')[1];
+  var setupxhr = new XMLHttpRequest();
+  setupxhr.open("GET", "/d2l/api/le/1.15/" + ou + "/grades/setup/");
+  setupxhr.onload = function () {
+    if (setupxhr.status == 200) {
+      setup = JSON.parse(setupxhr.response).GradingSystem;
+      getGradeObjs(setup, ou);
+    }
+  }
+  setupxhr.send();
+}
+
+var num = 0,
+  den = 0;
+
+function getGradeObjs(setup, ou) {
+  var grades;
+  var gradesxhr = new XMLHttpRequest();
+  gradesxhr.open("GET", "/d2l/api/le/1.15/" + ou + "/grades/values/myGradeValues/");
+  gradesxhr.onload = function () {
+    if (gradesxhr.status == 200) {
+      grades = JSON.parse(gradesxhr.response);
+      if (setup == "Weighted") {
+        weightedOps(grades, ou);
+      } else if (setup == "Points") {
+        pointsOps(grades, ou);
+      }
+      document.getElementById('close').insertAdjacentHTML('afterend', '<p>Please note that this calculation should be used for reference only, and the actual final calculated grade may vary slightly</p>')
+
+    }
+  }
+  gradesxhr.send();
+}
+
+function weightedOps(grades, ou) {
+  var catxhr = new XMLHttpRequest();
+  catxhr.open("GET", "/d2l/api/le/1.15/" + ou + "/grades/categories/");
+  catxhr.onload = function () {
+    if (catxhr.status == 200) {
+      var catgs = JSON.parse(catxhr.response);
+      catgs.forEach(function (catg) {
+        catg.Grades.forEach(function (gradeobj) {
+          /* Get the earned value and insert */
+          var value;
+          gradeobj.Earned = grades.filter(function (grade) {
+            return grade.GradeObjectIdentifier == gradeobj.Id;
+          })[0];
+          /* Popluate Table */
+          gradeobj.wgtAchieved = (gradeobj.Earned / gradeobj.MaxPoints) * (gradeobj.Weight) * (catg.Weight / 100);
+          if (gradeobj.Earned != null) {
+            gradeobj.Earned = gradeobj.Earned.PointsNumerator;
+            value = gradeobj.Earned;
+            num += gradeobj.wgtAchieved;
+          } else {
+            value = "<input class ='newGrade' type='text' onKeyUp='updateGrade()'>"
+          }
+          if (gradeobj.IsBonus === false) {
+            document.querySelector('#whatIf tbody').insertAdjacentHTML('beforeend', "<tr> <td>" + gradeobj.Name + "</td> <td class='value'>" + value + " / " + gradeobj.MaxPoints + "</td> </tr>");
+            den += (gradeobj.Weight * catg.Weight / 100);
+          } else {
+            document.querySelector('#whatIf tbody').insertAdjacentHTML('beforeend', "<tr> <td>" + gradeobj.Name + "</td> <td class='value extra'>" + value + " / " + gradeobj.MaxPoints + "</td> </tr>");
+          }
+        });
+      });
+      console.log(catgs);
+      localStorage.setItem('num', num);
+      localStorage.setItem('den', den);
+      document.getElementById('num').innerText = Math.round(num * 100) / 100;
+      document.getElementById('den').innerText = Math.round(den * 100) / 100;
+      document.getElementById('perc').innerText = Math.round(num / den * 100);
+      document.getElementById('grade').innerText = getGrade((num / den));
+      document.getElementById('close').insertAdjacentHTML('afterend', '<p><em>Attention!</em> Weighted grades schemes are not yet supported</p>')
+    }
+  }
+  catxhr.send();
+}
+
+function pointsOps(grades, ou) {
+  var gradeobjs;
   var gradeobjxhr = new XMLHttpRequest();
   gradeobjxhr.open("GET", "/d2l/api/le/1.15/" + ou + "/grades/");
   gradeobjxhr.onload = function () {
     if (gradeobjxhr.status == 200) {
       gradeobjs = JSON.parse(gradeobjxhr.response);
-
-      var gradesxhr = new XMLHttpRequest();
-      gradesxhr.open("GET", "/d2l/api/le/1.15/" + ou + "/grades/values/myGradeValues/");
-      gradesxhr.onload = function () {
-        if (gradesxhr.status == 200) {
-          grades = JSON.parse(gradesxhr.response);
-          var num = 0,
-            den = 0;
-          gradeobjs.forEach(function (obj) {
-            var value;
-            value = grades.filter(function (grade) {
-              return grade.GradeObjectIdentifier == obj.Id
-            })[0];
-            if (value != null) {
-              num += value.PointsNumerator;
-              if (obj.IsBonus === false) {
-                den += value.PointsDenominator;
-              }
-              value = value.PointsNumerator;
-            } else {
-              value = "<input class ='newGrade' type='text' onKeyUp='updateGrade()'>"
-            }
-            if (obj.IsBonus === false) {
-              document.querySelector('#whatIf tbody').insertAdjacentHTML('beforeend', "<tr> <td>" + obj.Name + "</td> <td class='value'>" + value + " / " + obj.MaxPoints + "</td> </tr>");
-            } else {
-              document.querySelector('#whatIf tbody').insertAdjacentHTML('beforeend', "<tr> <td>" + obj.Name + "</td> <td class='value extra'>" + value + " / " + obj.MaxPoints + "</td> </tr>");
-            }
-          })
-          localStorage.setItem('num', num);
-          localStorage.setItem('den', den);
-          document.getElementById('num').innerText = num;
-          document.getElementById('den').innerText = den;
-          document.getElementById('perc').innerText = Math.round(num / den * 100);
-          document.getElementById('grade').innerText = getGrade((num / den));
+      gradeobjs.forEach(function (obj) {
+        var value;
+        value = grades.filter(function (grade) {
+          return grade.GradeObjectIdentifier == obj.Id
+        })[0];
+        if (value != null) {
+          num += value.PointsNumerator;
+          if (obj.IsBonus === false) {
+            den += value.PointsDenominator;
+          }
+          value = value.PointsNumerator;
+        } else {
+          value = "<input class ='newGrade' type='text' onKeyUp='updateGrade()'>"
         }
-      }
-      gradesxhr.send();
+        if (obj.IsBonus === false) {
+          document.querySelector('#whatIf tbody').insertAdjacentHTML('beforeend', "<tr> <td>" + obj.Name + "</td> <td class='value'>" + value + " / " + obj.MaxPoints + "</td> </tr>");
+        } else {
+          document.querySelector('#whatIf tbody').insertAdjacentHTML('beforeend', "<tr> <td>" + obj.Name + "</td> <td class='value extra'>" + value + " / " + obj.MaxPoints + "</td> </tr>");
+        }
+      })
+      localStorage.setItem('num', num);
+      localStorage.setItem('den', den);
+      document.getElementById('num').innerText = num;
+      document.getElementById('den').innerText = den;
+      document.getElementById('perc').innerText = Math.round(num / den * 100);
+      document.getElementById('grade').innerText = getGrade((num / den));
     }
   }
   gradeobjxhr.send();
 }
 
 function closeWhatIf() {
-  document.getElementById('whatIf').parentNode.removeChild(document.getElementById('whatIf'))
+  document.getElementById('whatIf').style = "display:none";
 }
 
 function updateGrade() {
@@ -64,8 +139,8 @@ function updateGrade() {
     }
     num += Number(value.value);
   });
-  document.querySelector('#num').innerHTML = num;
-  document.querySelector('#den').innerHTML = den;
+  document.querySelector('#num').innerHTML = Math.round(num * 100) / 100;
+  document.querySelector('#den').innerHTML = Math.round(den * 100) / 100;
   document.getElementById('perc').innerText = Math.round(num / den * 100);
   document.getElementById('grade').innerText = getGrade((num / den));
 }
--- whatIf.js
+++ whatIf.js
@@ -1,27 +1,25 @@
 /*eslint-env browser*/
-if (window.location.pathname.split('/')[4] == "my_grades") {
-  document.querySelector('form').style = "position: relative";
-  document.querySelector('form').insertAdjacentHTML('afterbegin', "<a id='whatif' class='dhdg_1 vui-heading-2' style='position: absolute; right: 0; font-size: 1.5em;' onclick=whatIf() href='#'>What if?</a>");
+document.querySelector('form').style = "position: relative";
+document.querySelector('form').insertAdjacentHTML('afterbegin', "<a id='whatif' class='dhdg_1 vui-heading-2' style='position: absolute; right: 0; font-size: 1.5em;' onclick=whatIf() href='#'>What if?</a>");
 
-  var windowUrl = chrome.extension.getURL('window.html');
-  var xhr = new XMLHttpRequest();
-  var whatIfWindow;
-  xhr.open("GET", windowUrl);
-  xhr.onload = function (e) {
-    if (xhr.status == 200) {
-      whatIfWindow = xhr.response;
-      document.querySelector('.d2l-page-main').insertAdjacentHTML('beforeend', whatIfWindow);
-    } else {
-      console.log(e)
-    }
-
+var windowUrl = chrome.extension.getURL('window.html');
+var xhr = new XMLHttpRequest();
+var whatIfWindow;
+xhr.open("GET", windowUrl);
+xhr.onload = function (e) {
+  if (xhr.status == 200) {
+    whatIfWindow = xhr.response;
+    document.querySelector('.d2l-page-main').insertAdjacentHTML('beforeend', whatIfWindow);
+  } else {
+    console.log(e)
   }
-  xhr.send();
 
-  var script = document.createElement('script');
-  script.src = chrome.extension.getURL('scripts.js');
-  script.onload = function () {
-    this.remove();
-  };
-  (document.head || document.documentElement).appendChild(script);
 }
+xhr.send();
+
+var script = document.createElement('script');
+script.src = chrome.extension.getURL('scripts.js');
+script.onload = function () {
+  this.remove();
+};
+(document.head || document.documentElement).appendChild(script);
