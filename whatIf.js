5110df51e9361d0d87403a09ee201574c4e827d8
Ben Harker <har05054@byui.edu>
2017-03-31 13:59:46 -0600

    Added upcoming assignments widget

--- whatIf.js
+++ whatIf.js
@@ -1,25 +1,52 @@
 /*eslint-env browser*/
-document.querySelector('form').style = "position: relative";
-document.querySelector('form').insertAdjacentHTML('afterbegin', "<a id='whatif' class='dhdg_1 vui-heading-2' style='position: absolute; right: 0; font-size: 1.5em;' onclick=whatIf() href='#'>What if?</a>");
+if (top.location.pathname.split('/')[4] == 'my_grades') {
 
-var windowUrl = chrome.extension.getURL('window.html');
-var xhr = new XMLHttpRequest();
-var whatIfWindow;
-xhr.open("GET", windowUrl);
-xhr.onload = function (e) {
-  if (xhr.status == 200) {
-    whatIfWindow = xhr.response;
-    document.querySelector('.d2l-page-main').insertAdjacentHTML('beforeend', whatIfWindow);
-  } else {
-    console.log(e)
+  document.querySelector('form').style = "position: relative";
+  document.querySelector('form').insertAdjacentHTML('afterbegin', "<a id='whatif' class='dhdg_1 vui-heading-2' style='position: absolute; right: 0; font-size: 1.5em;' onclick=whatIf() href='#'>What if?</a>");
+
+  var windowUrl = chrome.extension.getURL('window.html');
+  var xhr = new XMLHttpRequest();
+  var whatIfWindow;
+  xhr.open("GET", windowUrl);
+  xhr.onload = function (e) {
+    if (xhr.status == 200) {
+      whatIfWindow = xhr.response;
+      document.querySelector('.d2l-page-main').insertAdjacentHTML('beforeend', whatIfWindow);
+    } else {
+      console.log(e)
+    }
+
   }
+  xhr.send();
 
+  var script = document.createElement('script');
+  script.src = chrome.extension.getURL('scripts.js');
+  script.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(script);
+
+} else if (top.location.pathname.split('/')[2] == 'home') {
+
+  var upcomingUrl = chrome.extension.getURL('upcoming.html');
+  var upcoming;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", upcomingUrl);
+  xhr.onload = function (e) {
+    if (xhr.status == 200) {
+      upcoming = xhr.response;
+      document.querySelectorAll('.d2l-homepage .d2l-box')[0].insertAdjacentHTML('afterbegin', upcoming);
+    } else {
+      console.log(e)
+    }
+
+  }
+  xhr.send();
+
+  var script = document.createElement('script');
+  script.src = chrome.extension.getURL('upcoming.js');
+  script.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(script);
 }
-xhr.send();
-
-var script = document.createElement('script');
-script.src = chrome.extension.getURL('scripts.js');
-script.onload = function () {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(script);
