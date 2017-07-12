6bf29cdb0db39475b936a03eb653f59f355ed51c
tallroids <har05054@byui.edu>
2017-07-12 16:54:43 -0600

    corrected display order and removed unnecessary logic

--- upcoming.js
+++ upcoming.js
@@ -47,36 +47,49 @@ function getCourseIds(courses) {
   return string;
 }
 
+function sortItems(items) {
+  return items.sort(function (a, b) {
+    return (new Date(Date.parse(a.DueDate)) - new Date(Date.parse(b.DueDate)))
+  })
+}
+
+function displayRows(items, classes) {
+  items.forEach(function (item) {
+    var itemRow = "<tr class='" + item.class + "'><td class='checkCont'><label for='" + item.ItemId + "'><input type='checkbox' id='" + item.ItemId + "' class='checkBoxes' " + item.isHidden + " onchange='updateVisibility(this)'></label></td><td><a href=" + item.ItemUrl + " title='" + item.ItemName + "' target='_blank'>" + item.ItemName + "</a></td>" + getCourse(item.OrgUnitId, classes) + "<td title='" + new Date(Date.parse(item.DueDate)).toLocaleString() + "'>" + new Date(Date.parse(item.DueDate)).toLocaleString() + "</td></tr>";
+    document.getElementById('upcomingTbody').insertAdjacentHTML('beforeend', itemRow)
+  })
+}
+
 function getItems(classes) {
   if (typeof (localStorage["d2l_assignments"]) === 'undefined') {
     localStorage['d2l_assignments'] = "{}";
   }
   var d2l_assignments = JSON.parse(localStorage['d2l_assignments']);
 
-  var items;
   var itemsxhr = new XMLHttpRequest();
   itemsxhr.open("GET", "/d2l/api/le/1.18/content/myItems/?completion=3&orgUnitIdsCSV=" + getCourseIds(classes) + "&startDateTime=" + itemsStartDate.toISOString() + "&endDateTime=" + itemsEndDate.toISOString());
   itemsxhr.onload = function () {
     if (itemsxhr.status == 200) {
-      items = JSON.parse(itemsxhr.response);
-      items.Objects.forEach(function (item) {
+      var items = JSON.parse(itemsxhr.response).Objects;
+      /*Loop through items*/
+      items.forEach(function (item) {
         if (typeof (d2l_assignments[item.ItemId]) === "undefined") {
           d2l_assignments[item.ItemId] = {
             id: item.ItemId,
             isHidden: false
           }
         }
+        item.class = "visible",
+          item.isHidden = "";
+        /* Check what type of item it is*/
 
-        var itemClass = "visible",
-          isHidden = "";
-        /* Check what typ of item it is*/
         if (item.DueDate === null) {
           /* if there is no due date, check if it is an availability */
           if (item.EndDate === null) {
             /* if there is no end date, set it as an availability notice */
             item.DueDate = item.StartDate;
             item.IsAvailability = true;
-            itemClass = "available"
+            item.class = "hidden"
           } else {
             /* if there is no due date, set it as the end date*/
             item.DueDate = item.EndDate;
@@ -86,112 +99,34 @@ function getItems(classes) {
         /* Check if the item is due today */
         if (Date.parse(item.DueDate) - currDate < 1000 * 60 * 60 * 24) {
           if (!item.IsAvailability) {
-            itemClass = "urgent";
+            item.class = "urgent";
           }
         }
 
         /* Check if item is an availability notice and hide it if it has passed */
         if (Date.parse(item.DueDate) - currDate < 0) {
           if (item.IsAvailability) {
-            itemClass = "oldAvailability";
+            item.class = "oldAvailability";
           } else {
-            itemClass = "late";
+            item.class = "late";
           }
         }
 
         if (typeof (d2l_assignments[item.ItemId]) != 'undefined') {
           if (d2l_assignments[item.ItemId].isHidden === true) {
-            itemClass += " hidden";
-            isHidden = "checked";
+            item.class += " hidden";
+            item.isHidden = "checked";
           }
         }
-
-        var itemRow = "<tr class='" + itemClass + "'><td class='checkCont'><label for='" + item.ItemId + "'><input type='checkbox' id='" + item.ItemId + "' class='checkBoxes' " + isHidden + " onchange='updateVisibility(this)'></label></td><td><a href=" + item.ItemUrl + " title='" + item.ItemName + "' target='_blank'>" + item.ItemName + "</a></td>" + getCourse(item.OrgUnitId, classes) + "<td title='" + new Date(Date.parse(item.DueDate)).toLocaleString() + "'>" + new Date(Date.parse(item.DueDate)).toLocaleString() + "</td></tr>";
-        document.getElementById('upcomingTbody').insertAdjacentHTML('beforeend', itemRow)
+        //        var itemRow = "<tr class='" + itemClass + "'><td class='checkCont'><label for='" + item.ItemId + "'><input type='checkbox' id='" + item.ItemId + "' class='checkBoxes' " + isHidden + " onchange='updateVisibility(this)'></label></td><td><a href=" + item.ItemUrl + " title='" + item.ItemName + "' target='_blank'>" + item.ItemName + "</a></td>" + getCourse(item.OrgUnitId, classes) + "<td title='" + new Date(Date.parse(item.DueDate)).toLocaleString() + "'>" + new Date(Date.parse(item.DueDate)).toLocaleString() + "</td></tr>";
+        //        document.getElementById('upcomingTbody').insertAdjacentHTML('beforeend', itemRow)
       });
+      displayRows(sortItems(items), classes)
       localStorage['d2l_assignments'] = JSON.stringify(d2l_assignments);
       document.getElementById('upcoming').insertAdjacentHTML('beforeend', "<p>*Assignments more than one week overdue are ommited. Quizzes with unlimited attempts will not disappear after completion</p>")
     }
   }
   itemsxhr.send();
-}
-/*make associative and get rid of this function!*/
-function isRecent(grade, d2l_grades) {
-  var courseId = grade.GradeObjectIdentifier;
-  var recent = true,
-    exists = false;
-  d2l_grades.forEach(function (gradeObj) {
-    if (gradeObj.id == courseId) {
-      exists = true;
-      var age = currDate - gradeObj.date;
-      recent = !(age > (100 * 60 * 60 * 24 * 2));
-    }
-  });
-  if (!exists) {
-    d2l_grades.push({
-      date: currDate.getTime(),
-      id: grade.GradeObjectIdentifier
-    });
-  }
-  return recent;
-}
-
-var gradeValues = [];
-
-function evaluateGrades(gradeValues) {
-  if (typeof (localStorage["d2l_grades"]) === 'undefined') {
-    localStorage['d2l_grades'] = "[]";
-  }
-  var d2l_grades = JSON.parse(localStorage['d2l_grades']);
-  var container = document.getElementById('gradesTbody');
-  gradeValues.forEach(function (course) {
-
-    var returnString;
-    var finalGrade = new XMLHttpRequest();
-    finalGrade.open("GET", "/d2l/api/le/1.18/" + course.Id + "/grades/final/values/myGradeValue", false);
-    finalGrade.onload = function () {
-      if (finalGrade.status == 200) {
-        var response = JSON.parse(finalGrade.response);
-        var gradePerc;
-        if (response.WeightedDenominator == null) {
-          gradePerc = Math.round(response.PointsNumerator / response.PointsDenominator * 100)
-        } else {
-          gradePerc = Math.round(response.WeightedNumerator / response.WeightedDenominator * 100)
-        }
-        returnString = "" + response.DisplayedGrade + " | " + gradePerc + "%";
-        return returnString;
-      }
-    }
-    finalGrade.send();
-
-    container.insertAdjacentHTML('beforeend', "<tr><th><a href='/d2l/lms/grades/my_grades/main.d2l?ou=" + course.Id + "'>" + course.name + "</a></th><th>" + returnString + "</th></tr>");
-    course.grades.forEach(function (grade) {
-      if (isRecent(grade, d2l_grades)) {
-        container.insertAdjacentHTML('beforeend', "<tr><td><a href='/d2l/lms/grades/my_grades/main.d2l?ou=" + course.Id + "'>" + grade.GradeObjectName + "</a></td><td>" + grade.DisplayedGrade + " | " + grade.PointsNumerator + "/" + grade.PointsDenominator + "</td></tr>");
-      }
-    });
-  })
-  localStorage["d2l_grades"] = JSON.stringify(d2l_grades);
-}
-
-function getGrades(courses) {
-  courses.forEach(function (course) {
-    var grades = new XMLHttpRequest();
-    grades.open("GET", "/d2l/api/le/1.15/" + course.Id + "/grades/values/myGradeValues/");
-    grades.onload = function () {
-      if (grades.status == 200) {
-        gradeValues.push({
-          name: course.Name,
-          Id: course.Id,
-          grades: JSON.parse(grades.response).reverse()
-        })
-        if (gradeValues.length == courses.length) {
-          evaluateGrades(gradeValues);
-        }
-      }
-    }
-    grades.send();
-  })
 }
 
 function getEnrollments() {
@@ -210,7 +145,6 @@ function getEnrollments() {
         }
       });
       getItems(filtered);
-      getGrades(filtered);
 
     }
   }
